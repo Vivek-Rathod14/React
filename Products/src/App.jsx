751eef1f2b961338e1f4537775678e3a34474bfa
Vivek-Rathod14 <rathodvivek5500@gmail.com>
2026-02-20 11:18:48 +0530

    Prodcut Update

--- Products/src/App.jsx
+++ Products/src/App.jsx
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Header  from '../components/Header/Header';
-import Footer from '../components/Fotter/fotter';
-import ProductHeader from '../components/ProductHeader';
-import Product from '../components/product';
+import Header  from './components/Header/Header';
+import Footer from './components/Fotter/fotter';
+import ProductHeader from './components/ProductHeader';
+import Product from './components/product';
 
 function App() {
   return (
