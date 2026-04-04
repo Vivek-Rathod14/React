603d6e4c968acc39f2a67a4063b5ea582b659c72
Vivek-Rathod14 <rathodvivek5500@gmail.com>
2026-04-04 17:44:45 +0530

    React-Exam

--- React-Exam/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
-import movieReducer from "./reducers/movieReducer";
-
-const store = createStore(movieReducer, applyMiddleware(thunk));
-
-export default store;
